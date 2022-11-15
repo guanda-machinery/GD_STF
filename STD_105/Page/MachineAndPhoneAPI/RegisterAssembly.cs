2a41a9500be39207ee6621728e0fe6c2c52a3957
hweiqi <107524981+hweiqi@users.noreply.github.com>
2022-11-15 17:30:41 +0800

    MachineAndPhoneAPI串接

---
+++ STD_105/Page/MachineAndPhoneAPI/RegisterAssembly.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MachineAndPhoneAPI
+{
+    public class RegisterAssembly
+    {
+        //加工組合清單之素材內容
+        public int errorCode { get; set; }
+
+        public class Data
+        {
+            public string id { get; set; }
+            public int errorCode { get; set; }
+
+            public string errorMessage { get; set; }
+        }
+        public List<Data> data { get; set; }
+    }
+}
