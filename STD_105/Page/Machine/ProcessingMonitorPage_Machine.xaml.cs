8631bef2f5d827c4204b21e67db97d2880d594d8
hank670608 <hank670608@gmail.com>
2022-11-16 08:17:11 +0800

    合併

--- STD_105/Page/Machine/ProcessingMonitorPage_Machine.xaml.cs
+++ STD_105/Page/Machine/ProcessingMonitorPage_Machine.xaml.cs
@@ -27,6 +27,9 @@ using WPFSTD105.ViewModel;
 using DevExpress.Xpf.Grid;
 using System.Windows.Controls.Primitives;
 using DevExpress.Xpf.Core.Native;
+using MouseButton = devDept.Eyeshot.MouseButton;
+using System.Web.UI.WebControls;
+using DevExpress.XtraSplashScreen;
 
 namespace STD_105
 {
@@ -34,12 +37,36 @@ namespace STD_105
     /// 新加工監控 ProcessingMonitorPage_Machine.xaml 的互動邏輯
     /// </summary>
     public partial class ProcessingMonitorPage_Machine : BasePage<ProcessingMonitorVM>
-    { 
+    {
+
+
+     //   public SplashScreenManager ScreenManager { get; set; } = SplashScreenManager.Create(() => new WaitIndicator(), new DevExpress.Mvvm.DXSplashScreenViewModel { });
+
+        public ObSettingVM ViewModel { get; set; } = new ObSettingVM();
+        /// <summary>
+        /// nc 設定檔
+        /// </summary>
+        //public NcTemp NcTemp { get; set; }
+        //public string DataPath { get; set; }
+        public List<List<object>> DataList { get; set; }
+
         public ProcessingMonitorPage_Machine()
         {
             InitializeComponent();
 
-         }
+            model.DataContext = ViewModel;
+            drawing.DataContext = ViewModel;
+            model.Unlock("UF20-HM12N-F7K3M-MCRA-FDGT");
+            drawing.Unlock("UF20-HM12N-F7K3M-MCRA-FDGT");
+            model.ActionMode = actionType.None;
+            DataList = new List<List<object>>();
+            drawing.LineTypes.Add(Steel2DBlock.LineTypeName, new float[] { 35, -35, 35, -35 });
+            model.Secondary = drawing;
+            drawing.Secondary = model;
+            //ControlDraw3D();
+            //CheckReportLogoExist();
+
+        }
         private void BasePage_Unloaded(object sender, RoutedEventArgs e)
         {
             if (DataContext is IDisposable disposable)
@@ -57,7 +84,28 @@ namespace STD_105
         }
 
 
-        
+        private void Model3D_Loaded(object sender, RoutedEventArgs e)
+        {
+            #region Model 初始化
+            //model.InitialView = viewType.Top;
+            /*旋轉軸中心設定當前的鼠標光標位置。 如果模型全部位於相機視錐內部，
+             * 它圍繞其邊界框中心旋轉。 否則它繞著下點旋轉鼠標。 如果在鼠標下方沒有深度，則旋轉發生在
+             * 視口中心位於當前可見場景的平均深度處。*/
+            //model.Rotate.RotationCenter = rotationCenterType.CursorLocation;
+            //旋轉視圖 滑鼠中鍵 + Ctrl
+            model.Rotate.MouseButton = new MouseButton(mouseButtonsZPR.Middle, modifierKeys.Ctrl);
+            //平移滑鼠中鍵
+            model.Pan.MouseButton = new MouseButton(mouseButtonsZPR.Middle, modifierKeys.None);
+            model.ActionMode = actionType.SelectByBox;
+            if (ViewModel.Reductions == null)
+            {
+                ViewModel.Reductions = new ReductionList(model, drawing); //紀錄使用找操作
+            }
+
+            #endregion
+        }
+
+
         private void ScrollOwner_ScrollChanged(object sender, ScrollChangedEventArgs e)
         {
             if ((sender as DevExpress.Xpf.Grid.TableView).Name == MachiningCombinational_List_TableView.Name)
