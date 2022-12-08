6db940ae6d86a1576c80aeb76ef1b50119e60191
hank670608 <hank670608@gmail.com>
2022-12-08 09:04:09 +0800

    合併

--- WPFSTD105Base/Model/Expand.cs
+++ WPFSTD105Base/Model/Expand.cs
@@ -116,11 +116,13 @@ namespace WPFSTD105.Model
             // 依序取出素材零件位置
             for (int i = 0; i < place.Count; i++)
             {
-                if (place.Count == 1) // count=1表素材沒有零件組成,只有(前端切除)程序紀錄
+                if (place.Count == 1) // count=1表示素材沒有零件組成,只有(前端切除)程序紀錄
                 {
                     return;
                 }
 
+                if (place[i].End == 0) // 素材前端切除設為0時 ,不打點
+                    continue;
 
                 if (place[i].IsCut) //如果是切割物件
                 {
@@ -129,6 +131,9 @@ namespace WPFSTD105.Model
                     {
                         entities.Add(cut1);
                     }
+
+
+
 
                     MyCs myCs = new MyCs();
                     ObservableCollection<SplitLineSettingClass> ReadSplitLineSettingData = ser.GetSplitLineData();                          //  備份當前加工區域數值
