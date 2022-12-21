29bf47970823e6bd9d9a8ddf5593c662b50f0e22
SiegfriedR <aeonsiegfried@gmail.com>
2022-12-21 09:07:30 +0800

    20221221 修正製品設品設定phase及ShippingNumber不可為空值的問題

---
+++ STD_105/ValueConverters/NullableIntConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFWindowsBase;
+
+namespace STD_105
+{
+    internal class NullableIntConverter: BaseValueConverter<NullableIntConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var nullable = value as int?;
+            var result = string.Empty;
+
+            if (nullable.HasValue)
+            {
+                result = nullable.Value.ToString();
+            }
+
+            return result;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = value as string;
+            int intValue;
+            int? result = null;
+
+            if (int.TryParse(stringValue, out intValue))
+            {
+                result = new Nullable<int>(intValue);
+            }
+
+            return result;
+        }
+    }
+}
